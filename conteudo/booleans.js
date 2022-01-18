cab91772fc009fa64ccaa87b544adb25e03c11f9
PHELIPE STERING <68488889+phelipestering@users.noreply.github.com>
2022-01-18 18:24:46 -0300

    teste booleans

--- conteudo/booleans.js
+++ conteudo/booleans.js
@@ -5,3 +5,20 @@ const inspet2 = 'Daniel';
 let cadastroAtivo = true;
 
 console.log(inspet1 != inspet2);
+
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+
+
+let input = "Phelipe";
+
+if (input === null) {
+    console.log('não há informação');
+} else {
+    console.log(input);
+}
+
