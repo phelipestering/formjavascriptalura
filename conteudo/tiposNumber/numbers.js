c5f63d46b81a753ac944840d9f39279dd64687e3
PHELIPE STERING <68488889+phelipestering@users.noreply.github.com>
2022-01-18 18:11:20 -0300

    BOOLEAN

---
+++ conteudo/tiposNumber/numbers.js
@@ -0,0 +1,24 @@
+const meuNumero = 3;
+
+const num1 = 4;
+const num2 =5;
+
+const opMat = num1 + num2;
+
+console.log(opMat);
+
+// ponto flutuante
+
+const numPontFlu = 3.3;
+
+const numPontFlu2 = .5;
+
+const opMat2 = numPontFlu + numPontFlu2
+
+console.log(opMat2);
+
+// trabalhando com NAN
+
+const alura = "Alura";
+
+console.log
---
+++ conteudo/tiposStrings/tiposStrings.js
@@ -0,0 +1,10 @@
+const texto1 = "Phelipe Stering Lima";
+
+const texto2 = "Valeria Dias";
+
+//concat
+
+const textConcat = texto1 + texto2
+
+console.log(textConcat)
+
